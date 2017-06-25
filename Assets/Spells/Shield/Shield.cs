95d18c0cc218e8125b8610bdda8938272ce7127e
Anton Josefsson <anton.mattias.josefsson@gmail.com>
2017-06-25 10:02:53 +0200

    Upgraded Shield

--- Assets/Spells/Shield/Shield.cs
+++ Assets/Spells/Shield/Shield.cs
@@ -1,7 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.Networking;
 
-public class Shield : MonoBehaviour {
+public class Shield : NetworkBehaviour {
 
 	public Spell spell;
 	public GameObject owner;
@@ -16,7 +17,11 @@ public class Shield : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		AudioSource.PlayClipAtPoint(cast, transform.position);
-		GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+
+        if (!isServer)
+            return;
+
+        GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
 		string ownerName = spell.owner;
 		foreach(GameObject player in players)
 		{
@@ -33,28 +38,28 @@ public class Shield : MonoBehaviour {
 		spell.Invoke ("KillSelf", duration);
 
 		
-		if(GetComponent<NetworkView>().isMine)
-		{
-			Upgrading upgrading = GameObject.Find ("GameHandler").GetComponent<Upgrading>();
-			if(upgrading.shieldAmp.currentLevel > 0)
-			{
-				GetComponent<NetworkView>().RPC ("ActivateAmplify", RPCMode.All, upgrading.shieldAmp.currentLevel);
+		//if(GetComponent<NetworkView>().isMine)
+		//{
+		//	Upgrading upgrading = GameObject.Find ("GameHandler").GetComponent<Upgrading>();
+		//	if(upgrading.shieldAmp.currentLevel > 0)
+		//	{
+		//		GetComponent<NetworkView>().RPC ("ActivateAmplify", RPCMode.All, upgrading.shieldAmp.currentLevel);
 				
-				if(upgrading.shieldAim.currentLevel > 0)
-				{
-					GetComponent<NetworkView>().RPC ("ActivateAim", RPCMode.All);
-				}
-			}
+		//		if(upgrading.shieldAim.currentLevel > 0)
+		//		{
+		//			GetComponent<NetworkView>().RPC ("ActivateAim", RPCMode.All);
+		//		}
+		//	}
 			
-			if(upgrading.shieldCd.currentLevel > 0)
-			{
-				//networkView.RPC ("DecreaseCd", RPCMode.All, upgrading.shieldCd.currentLevel);
-				if(upgrading.shieldAbsorb.currentLevel > 0)
-				{
-					GetComponent<NetworkView>().RPC ("ActivateAbsorb", RPCMode.All, upgrading.shieldAbsorb.currentLevel);
-				}
-			}
-		}
+		//	if(upgrading.shieldCd.currentLevel > 0)
+		//	{
+		//		//networkView.RPC ("DecreaseCd", RPCMode.All, upgrading.shieldCd.currentLevel);
+		//		if(upgrading.shieldAbsorb.currentLevel > 0)
+		//		{
+		//			GetComponent<NetworkView>().RPC ("ActivateAbsorb", RPCMode.All, upgrading.shieldAbsorb.currentLevel);
+		//		}
+		//	}
+		//}
 	}
 
 	[RPC]
@@ -83,6 +88,9 @@ public class Shield : MonoBehaviour {
 
 	void OnTriggerEnter2D(Collider2D other)
 	{
+        if (!isServer)
+            return;
+
 		if(other.CompareTag ("Spell"))
 		{
 			Spell otherSpell = (Spell)other.GetComponent("Spell");
@@ -90,11 +98,13 @@ public class Shield : MonoBehaviour {
 			{
 				if(otherSpell.type == Spell.spellType.Projectile)
 				{
-					Network.Instantiate(shieldHit, other.transform.position, Quaternion.identity, 0);
+					GameObject hitEffect = Instantiate(shieldHit, other.transform.position, Quaternion.identity);
+                    NetworkServer.Spawn(hitEffect);
+
 					if(absorbAmount > 0)
 					{
 						owner.GetComponent<DamageSystem>().Damage (-otherSpell.damage * absorbAmount, 0, transform.position, spell.owner);
-						Network.Destroy(otherSpell.gameObject);
+						Destroy(otherSpell.gameObject);
 					}
 					else
 					{
@@ -105,12 +115,12 @@ public class Shield : MonoBehaviour {
 						otherSpell.team = spell.team;
 						otherSpell.damage *= amplifyAmount;
 
-						if(reflectAim && GetComponent<NetworkView>().isMine)
-						{
-							Vector3 aim = Camera.main.ScreenToWorldPoint (new Vector3((int)Input.mousePosition.x, (int)Input.mousePosition.y, 0));
-							reflected = Vector3.Normalize(new Vector3(aim.x, aim.y) - transform.position);
-							otherSpell.GetComponent<NetworkView>().RPC ("SetAim", RPCMode.All, reflected.x, reflected.y, spell.team, amplifyAmount, transform.position);
-						}
+						//if(reflectAim && GetComponent<NetworkView>().isMine)
+						//{
+						//	Vector3 aim = Camera.main.ScreenToWorldPoint (new Vector3((int)Input.mousePosition.x, (int)Input.mousePosition.y, 0));
+						//	reflected = Vector3.Normalize(new Vector3(aim.x, aim.y) - transform.position);
+						//	otherSpell.GetComponent<NetworkView>().RPC ("SetAim", RPCMode.All, reflected.x, reflected.y, spell.team, amplifyAmount, transform.position);
+						//}
 					}
 				}
 			}
